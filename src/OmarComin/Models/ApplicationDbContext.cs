af1ff820f53d78e59c594ca3a2270be2b56f15f6
Charles Ewel and Charlie Baxter <charles+charlie@>
2016-09-27 11:26:38 -0700

    return results of a Google Places API call to the console

---
+++ src/OmarComin/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GoogleMapsPractice.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<LocationSearch> SavedSearches { get; set; }
+    }
+}
---
+++ src/OmarComin/Models/LocationSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace GoogleMapsPractice.Models
+{
+    [Table("Sightings")]
+    public class LocationSearch
+    {
+        [Key]
+        public int Id { get; set; }
+        public float Lat { get; set; }
+        public float Long { get; set; }
+        public string Description { get; set; }
+
+        public void Search()
+        {
+            var client = new RestClient("https://maps.googleapis.com/maps/api/place");
+            var request = new RestRequest("/nearbysearch/json?location=" + Lat + "," + Long + "&radius=1000&name=" + Description + "&key=AIzaSyCVSYAYFCgQRLTaigD-y5QZfoZj_LhbbWU", Method.POST);
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+        }
+    }  
+}
