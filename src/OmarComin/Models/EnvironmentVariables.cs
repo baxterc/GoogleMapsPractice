8ec3c22c95b601ad2f1a70a65bdeaf884a148838
Charles Ewel and Charlie Baxter <charles+charlie@>
2016-09-27 11:40:17 -0700

    add an embedded google map

---
+++ src/OmarComin/Models/EnvironmentVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleMapsPractice.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string GMapsApiKey = "AIzaSyCVSYAYFCgQRLTaigD-y5QZfoZj_LhbbWU";
+    }
+}
